cc7ef6d412fe0c0d9e97d884567cd60839f84e13
Sam Donohue <Sam@null-040cced428d8.wufi.wustl.edu>
2013-01-25 16:26:30 -0600

    work

--- poissonGen.py
+++ poissonGen.py
@@ -1,16 +1,16 @@
 import sys
 
-if len(sys.argv) > 5:
-	print 'Too many arguments'
+if len(sys.argv) 1= 5:
+	print 'Wrong Number of Arguments'
+    print '[avgArrivalRate, avgStay, avgChargeNeed, timestep, interval]'
 	sys.exit()
-if len(sys.argv) < 5:
-	print 'Too few arguments'
-	sys.exit()
+
+
 
 class Vehicle:
 	numVehicles = 0
 
-	def __init__(self, arrivalTime, depTime, chargeNeed)
+	def __init__(self, arrivalTime, depTime, chargeNeed):
 		self.id = numVehicles
 		numVehicles++
 		self.arrivalTime = arrivalTime
@@ -18,4 +18,16 @@ class Vehicle:
 		self.chargeNeed = chargeNeed
 
 	def getInfo(self):
-		
+		return [self.arrivalTime, self.depTime, self.chargeNeed]
+
+avgArrivalRate = sys.argv[0]
+avgStay = sys.argv[1]
+avgChargeNeed = sys.argv[2]
+timestep = sys.argv[3]
+interval = sys.argv[4]
+
+
+
+
+
+
