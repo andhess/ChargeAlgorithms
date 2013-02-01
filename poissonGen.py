956a4a91eb3de6a1969f705e562bd8436a52ba42
Sam Donohue <Sam@null-040cced428d8.wufi.wustl.edu>
2013-02-01 14:34:57 -0600

    2/1

--- poissonGen.py
+++ poissonGen.py
@@ -17,31 +17,40 @@ interval = float(sys.argv[5])
 class Vehicle:
     numVehicles = 0
 
-    def __init__(self, arrivalTime, depTime, chargeNeed):
+    def __init__(self, arrivalTime, depTime, chargeNeed, currentCharge, chargeRate, maxCapacity):
         self.id = numVehicles
         numVehicles+=1
         self.arrivalTime = arrivalTime
         self.depTime = depTime
         self.chargeNeed = chargeNeed
+        self.currentCharge = currentCharge
+        self.chargeRate = chargeRate
+        self.maxCapacity = maxCapacity
 
     def getInfo(self):
-        return [self.arrivalTime, self.depTime, self.chargeNeed]
+        return [self.arrivalTime, self.depTime, self.chargeNeed, self.currentCharge, self.chargeRate, self.maxCapacity]
+
+def simulateFCFS(vehicles):
+    
+
+
 
 
 def simulateInterval():
     arrivalTimes = []
     prevArrival = 0
     while True:
-        nextArrival = vehicleArrives(prevArrival)
+        nextArrival = math.floor(vehicleArrives(prevArrival))
         if nextArrival > interval:
             break
         arrivalTimes.append(nextArrival)
         prevArrival = nextArrival
-    arrivalsPerTimestep = [0] * int(math.ceil(interval / timestep))    
-    for arrivalTime in arrivalTimes:
-        arrivalsPerTimestep[int(math.floor(arrivalTime/timestep))]+=1
-    print 'total arrivals = ',sum(arrivalsPerTimestep)
-    return arrivalsPerTimestep
+    # arrivalsPerTimestep = [0] * int(math.ceil(interval / timestep))    
+    # for arrivalTime in arrivalTimes:
+    #     arrivalsPerTimestep[int(math.floor(arrivalTime/timestep))]+=1
+    # print 'total arrivals = ',sum(arrivalsPerTimestep)
+    #return arrivalsPerTimestep
+    return arrivalTimes
 
 
 def vehicleArrives(prevArrival):
