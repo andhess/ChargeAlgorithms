cda6b1dee275103251db757beb727127a92fe58b
andhess <andrewclaytonhess@gmail.com>
2013-03-21 11:12:18 -0500

    bug fix on timeToCharge

--- vehicle.py
+++ vehicle.py
@@ -31,7 +31,7 @@ class Vehicle:
         self.depTime             =     depTime
         self.chargeRate          =     chargeRate
         self.maxCapacity         =     maxCapacity
-        self.timeToCharge        =     math.ceil( self.chargeNeeded - self.currentCharge ) / chargeRate  #linear
+        self.timeToCharge        =     math.ceil( ( ( self.chargeNeeded - self.currentCharge ) * 1.0 ) / chargeRate )  #linear
         self.totalTime           =     depTime - arrivalTime
         self.freeTime            =     self.totalTime - self.timeToCharge
         self.laxity              =     self.freeTime / self.totalTime
